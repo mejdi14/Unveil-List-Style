3e082683935b86229c15d36a9745d0e5fccb985b
mejdi14 <mejdiclubiste@gmail.com>
2024-05-21 11:25:37 +0200

    init project

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,48 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import unveilliststyle.composeapp.generated.resources.Res
+import unveilliststyle.composeapp.generated.resources.compose_multiplatform
+import unveilliststyle.composeapp.generated.resources.the_killing_poster
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy((-124).dp),
+            ) {
+                for (i in 1..10) {
+                    item {
+                        Image(painter = painterResource(Res.drawable.the_killing_poster),
+                            null, modifier = Modifier.size(200.dp).graphicsLayer {
+                                rotationZ = 20f
+                            })
+                    }
+                }
+            }
+        }
+    }
+}
